91755cb33789eb10d59065044e36486855a81e2a
Darren <darren.lo.0530@gmail.com>
2020-09-10 18:54:17 -0400

    Added relation between user and friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #setting up friends
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, foreign_key: :friend_id, class_name: "Friendship"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  def all_friends
+    friends.or(inverse_friends)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_054354) do
+ActiveRecord::Schema.define(version: 2020_09_10_220604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_054354) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
